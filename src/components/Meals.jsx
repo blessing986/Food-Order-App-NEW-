240a2dfb3603d93266764ffe86e538b3536eb4b9
Blessing U <blessing986@users.noreply.github.com>
2023-11-10 09:25:57 +0000

    chore: adding a meal item component

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import MealItem from "./MealItem";
 
 export default function Maeals() {
   const [loadedMeals, setLoadedMeals] = useState([]);
@@ -20,7 +21,7 @@ export default function Maeals() {
   return (
     <ul id="meals">
       {loadedMeals.map((meal) => (
-        <li key={meal.id}>{meal.name}</li>
+        <MealItem key={meal.id} meal={meal}/>
       ))}
     </ul>
   );
